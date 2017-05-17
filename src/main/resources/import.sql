2a95609cfd387f81a2b536f41e1c9ad9741237ac
S87345 <robertas.vilkisius@seb.se>
2017-05-17 08:55:18 +0200

    fixed import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,9 +1,9 @@
 INSERT INTO Customer (id, firstName, lastName, email, phoneNumber) VALUES (44042469040005, 'Pennie', 'Gorevan', '28@sebanken.nu', '08004041')
 INSERT INTO Customer (id, firstName, lastName, email, phoneNumber) VALUES (45012236180005, 'Rob', 'Schule', 'schulerob@gmail.com', '37061743092')
 INSERT INTO Customer (id, firstName, lastName, email, phoneNumber) VALUES (40045667840005, 'Mikael', 'Olson', 'mikaleolson@gmail.com', '08004042')
-INSERT INTO Customer (id, firstName, lastName, email, phoneNumber) VALUES (44745788860005, 'Long', 'Friel', '29@sebanken.nu', '08004043')
+INSERT INTO Customer (id, firstName, lastName, email, phoneNumber) VALUES (44745788860005, 'Gab', 'Bausk', 'gabriele.gelumbauskaite@gmail.com', '08004043')
 INSERT INTO Customer (id, firstName, lastName, email, phoneNumber) VALUES (34664678960005, 'Taher', 'Brereton', '30@sebanken.nu', '08004044')
-INSERT INTO Customer (id, firstName, lastName, email, phoneNumber) VALUES (46567889990005, 'Northway', 'Medicinos centras', 'info@nmc.lt', '37061743092')
+INSERT INTO Customer (id, firstName, lastName, email, phoneNumber) VALUES (46567889990005, 'Northway', 'Medicinos centras', 'info@nmc.lt', '37061743092)'
 
 INSERT INTO Account (customerId, balance) VALUES (44042469040005, 1620.80)
 INSERT INTO Account (customerId, balance) VALUES (45012236180005, 3325.40)
