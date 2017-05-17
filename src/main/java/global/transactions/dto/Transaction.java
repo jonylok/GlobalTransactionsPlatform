ac85637277ecd99fc615349165d992b5b398858a
Petrauskas <S76076@corp1.ad1.seb.net>
2017-05-17 08:13:47 +0200

    added transaction dto

---
+++ src/main/java/global/transactions/dto/Transaction.java
@@ -0,0 +1,55 @@
+package global.transactions.dto;
+
+import java.math.BigDecimal;
+
+public class Transaction {
+
+	private String id;
+	private String code;
+	private String from;
+	private String to;
+	private BigDecimal amount;
+	
+	public Transaction() {
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getFrom() {
+		return from;
+	}
+
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+	public String getTo() {
+		return to;
+	}
+
+	public void setTo(String to) {
+		this.to = to;
+	}
+
+	public BigDecimal getAmount() {
+		return amount;
+	}
+
+	public void setAmount(BigDecimal amount) {
+		this.amount = amount;
+	}	
+}
--- src/main/java/global/transactions/rest/TransactionAPI.java
+++ src/main/java/global/transactions/rest/TransactionAPI.java
@@ -1,12 +1,11 @@
 package global.transactions.rest;
 
-import java.math.BigDecimal;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,15 +18,22 @@ public class TransactionAPI {
 	@Autowired
 	private TransactionService transactionService;
 	
+//	@CrossOrigin
+//	@RequestMapping(value = "Transaction/{id}/{code}/{from}/{to}/{amount}/", method = { RequestMethod.PUT })
+//	public ResponseEntity<String> transactionExecute(@PathVariable String id, @PathVariable String code, @PathVariable String from, @PathVariable String to, @PathVariable BigDecimal amount) {
+//		Boolean transactionExecuted = transactionService.executeTransaction(id, code, from, to, amount);
+//		return transactionExecuted==true?new ResponseEntity<>(HttpStatus.CREATED):new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+//	}
+	
 	@CrossOrigin
-	@RequestMapping(value = "Transaction/{id}/{code}/{from}/{to}/{amount}/", method = { RequestMethod.PUT })
-	public ResponseEntity<String> transactionExecute(@PathVariable String id, @PathVariable String code, @PathVariable String from, @PathVariable String to, @PathVariable BigDecimal amount) {
-		Boolean transactionExecuted = transactionService.executeTransaction(id, code, from, to, amount);
+	@RequestMapping(value = "/Transaction", method = { RequestMethod.PUT })
+	public ResponseEntity<String> transactionExecute(@RequestBody global.transactions.dto.Transaction transaction) {
+		Boolean transactionExecuted = transactionService.executeTransaction(transaction.getId(), transaction.getCode(), transaction.getFrom(), transaction.getTo(), transaction.getAmount());
 		return transactionExecuted==true?new ResponseEntity<>(HttpStatus.CREATED):new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
 	}
 
 	@CrossOrigin
-	@RequestMapping(value = "Transaction/{from}/", method = { RequestMethod.POST })
+	@RequestMapping(value = "/Transaction/{from}/", method = { RequestMethod.POST })
 	public ResponseEntity<String> transactionCreate(@PathVariable String from) {
 		String transactionId = transactionService.createTransaction(from);
 		ResponseEntity<String> response = new ResponseEntity<String>(transactionId, HttpStatus.CREATED);
